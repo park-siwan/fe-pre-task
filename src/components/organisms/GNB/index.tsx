bf184124d8d8591b39560520b6d26d147165d982
siwan <pswan3168@gmail.com>
2022-01-24 23:43:21 +0900

    GNB 컴포넌트 생성

---
+++ src/components/organisms/GNB/index.tsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import Typography from "../../atoms/Typography";
+import { Link } from "react-router-dom";
+const Header = styled.header`
+  height: 70px;
+  background-color: rgb(0, 0, 0);
+`;
+const Wraper = styled.div`
+  height: 100%;
+  display: flex;
+  -webkit-box-pack: justify;
+  justify-content: space-between;
+  -webkit-box-align: center;
+  align-items: center;
+  padding: 0px 20px;
+`;
+export default function GNB() {
+  return (
+    <Header>
+      <Wraper>
+        <nav>
+          <Link to="/">
+            <Typography variant="h1" color="white">
+              Motionlabs
+            </Typography>
+          </Link>
+        </nav>
+      </Wraper>
+    </Header>
+  );
+}
