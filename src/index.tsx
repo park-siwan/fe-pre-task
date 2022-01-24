2b2280871529cd82b0f8adacf03b166aa484fddc
siwan <pswan3168@gmail.com>
2022-01-24 23:41:15 +0900

    스타일 리셋완료

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import "./styles/reset.css";
+import "./styles/nomalize.css";
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
