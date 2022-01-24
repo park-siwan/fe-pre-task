4e2e43fc0068f1188c0811f26a1c89ce4145ec42
siwan <pswan3168@gmail.com>
2022-01-24 23:42:55 +0900

    Typography 컴포넌트 생성

---
+++ src/components/atoms/Typography/index.tsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  color: ${(props) => props.color || "rgb(36, 36, 36)"};
+  font-size: 24px;
+`;
+const H2 = styled.h2``;
+
+type TypographyProps = {
+  variant: string;
+  children: string;
+  color?: string;
+};
+
+const Typography = ({ variant, children, color }: TypographyProps): any => {
+  switch (variant) {
+    case "h1":
+      return <H1 color={color}>{children}</H1>;
+    case "h2":
+      return <H2 color={color}>{children}</H2>;
+    default:
+      <p>{children}</p>;
+  }
+};
+export default Typography;
