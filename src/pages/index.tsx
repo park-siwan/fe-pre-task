bf7380cc1e3062986baaca1a461b7a4316da9c9d
siwan <pswan3168@gmail.com>
2022-01-24 21:45:49 +0900

    router v6 설정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,17 @@
 import Layout from "../Layout";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import Report from "./Report";
+import Passenger from "./PassengerList";
 
 export default function Root() {
-  return <Layout>레이아웃 구조잡기</Layout>;
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/report" element={<Report />} />
+        <Route path="/passenger" element={<Passenger />} />
+      </Routes>
+    </Layout>
+  );
 }
