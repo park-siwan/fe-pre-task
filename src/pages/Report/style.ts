e0f3aefa4369742282fd3c1a078e463f9d4a8ae1
siwan <pswan3168@gmail.com>
2022-01-29 17:29:24 +0900

    cricle svg용 styled component정의

---
+++ src/pages/Report/style.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Svg = styled.svg`
+  height: 160px;
+  width: 100%;
+`;
+export const Circle = styled.circle`
+  r: 4.5;
+  fill: #222;
+`;
+export const Text = styled.text`
+  fill: rgb(112, 112, 112);
+  font-size: 12px;
+  font-weight: bold;
+`;
+export const Line = styled.line`
+  stroke: #222;
+  stroke-width: "2";
+`;
