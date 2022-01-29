23b6faeb6b41c8668087aeffb695a999a82337a1
siwan <pswan3168@gmail.com>
2022-01-30 02:07:33 +0900

    fix: BarChart 일수 위치 수정

--- src/pages/Report/BarChart/index.tsx
+++ src/pages/Report/BarChart/index.tsx
@@ -7,16 +7,15 @@ export default function BarChart({ data }: dataProps) {
     return (
       <>
         {data.map(({ period, startDate, maxPeriod }, index: number) => {
-          const RELATIVE_RATIO = 100 * (period / maxPeriod);
+          const RELATIVE_RATIO = 100 * (period / maxPeriod); //상대비율 계산
+          const REVERSE_AXIS_Y = 100 - RELATIVE_RATIO; //Y축 반전
           return (
             <Group x={START_POINT * (index + 1)} y={30} key={period}>
-              <Text y={-10}>{period}일</Text>
-              <Bar
-                x={-15}
-                y={100 - RELATIVE_RATIO}
-                height={RELATIVE_RATIO}
-              ></Bar>
-              <Text y={125}>{format(new Date(startDate), "MM/dd")}</Text>
+              <Text y={REVERSE_AXIS_Y - 10}>{period}일</Text>
+              <Bar x={-15} y={REVERSE_AXIS_Y} height={RELATIVE_RATIO}></Bar>
+              <Text y={125} fontWeight="500">
+                {format(new Date(startDate), "MM/dd")}
+              </Text>
             </Group>
           );
         })}
