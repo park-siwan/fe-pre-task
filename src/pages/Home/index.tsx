a7af9b43a32d1cf9c16c8e7aa16fec7d9ea95553
siwan <pswan3168@gmail.com>
2022-01-29 14:37:28 +0900

    홈 화면 제작

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,33 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Wrap = styled.div`
+  padding: 40px;
+`;
+
+const Ul = styled.ul`
+  padding-left: 40px;
+`;
+const Li = styled.li`
+  margin: 40px 0px;
+  list-style-type: disc;
+  text-decoration: underline;
+  font-weight: bold;
+  a {
+    font-size: 18px;
+  }
+`;
 export default function Home() {
   return (
-    <ul>
-      <li>
-        <Link to="/report">레포트</Link>
-      </li>
-      <li>
-        <Link to="/passenger">승객목록</Link>
-      </li>
-    </ul>
+    <Wrap>
+      <Ul>
+        <Li>
+          <Link to="/report">레포트</Link>
+        </Li>
+        <Li>
+          <Link to="/passenger">승객목록</Link>
+        </Li>
+      </Ul>
+    </Wrap>
   );
 }
