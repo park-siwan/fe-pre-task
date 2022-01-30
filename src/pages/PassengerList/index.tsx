fe67d5d5c906e208556ca01fa0a3d185951562d1
siwan <pswan3168@gmail.com>
2022-01-30 13:08:06 +0900

    passenger 기본구조잡기

--- src/pages/PassengerList/index.tsx
+++ src/pages/PassengerList/index.tsx
@@ -1,3 +1,34 @@
-export default function index() {
-  return <div>승객목록</div>;
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+export default function PassengerList() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const url = "https://api.instantwebtools.net/v1/passenger?page=0&size=10";
+    axios.get(url).then((response) => {
+      let {
+        data: { data },
+      } = response;
+
+      setData(data);
+      console.log(data);
+    });
+  }, []);
+
+  if (typeof data === "undefined") {
+    return null;
+  }
+
+  interface itemList {
+    name: string;
+    _id: number;
+  }
+
+  return (
+    <div>
+      {data.map(({ _id, name }: itemList) => (
+        <div key={_id}>{name}</div>
+      ))}
+    </div>
+  );
 }
